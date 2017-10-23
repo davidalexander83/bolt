eb85f7da1fb95c5a3999c0e4308d12aff1f6219e
davidalexander83 <david.alexander@puppet.com>
2017-10-23 13:27:23 +1100

    typo in gethost

--- modules/demo/tasks/gethost.py
+++ modules/demo/tasks/gethost.py
@@ -6,7 +6,7 @@ import os
 host = os.environ.get('PT_host')
 
 if host:
-    print("%s is available at %s on %s" % (host, socket,gethostbyname(host), socket.gethostname()))
+    print("%s is available at %s on %s" % (host, socket.gethostbyname(host), socket.gethostname()))
 else:
     print('No host argument passed')
     sys.exit(1)
