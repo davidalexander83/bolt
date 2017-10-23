35ae6c8c48a8ff9a3a24174b840a80f743525180
davidalexander83 <david.alexander@puppet.com>
2017-10-23 13:25:38 +1100

    added gethost.py

---
+++ modules/demo/tasks/gethost.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import socket
+import sys
+import os
+
+host = os.environ.get('PT_host')
+
+if host:
+    print("%s is available at %s on %s" % (host, socket,gethostbyname(host), socket.gethostname()))
+else:
+    print('No host argument passed')
+    sys.exit(1)
